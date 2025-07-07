2ec2c8b83a44da59e6e63858dfc20cf17435386e
Thilo Wilts <thilowilts@googlemail.com>
2025-07-07 15:07:09 +0200

    Filter rule results table by selected rules

--- frontend/src/components/dashboard/RuleResultsTable.tsx
+++ frontend/src/components/dashboard/RuleResultsTable.tsx
@@ -9,44 +9,51 @@ interface Props {
   onSelectedRulesChange: (v: string[]) => void;
 }
 
-const RuleResultsTable: React.FC<Props> = ({ results, allRules, selectedRules, onSelectedRulesChange }) => (
-  <div>
-    <h3 className="text-xl font-semibold mb-2">Latest Rule Results</h3>
-    <Autocomplete
-      multiple
-      size="small"
-      options={allRules}
-      value={selectedRules}
-      onChange={(_, v) => onSelectedRulesChange(v as string[])}
-      renderInput={(params) => <TextField {...params} label="Filter rules" placeholder="All" />}
-      sx={{ mb: 1, width: 300 }}
-    />
-    <TableContainer component={Paper}>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Detected At</TableCell>
-            <TableCell>Rule Name</TableCell>
-            <TableCell>Result</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {results.map(r => (
-            <TableRow key={r.id}>
-              <TableCell>{new Date(r.detected_at).toLocaleString()}</TableCell>
-              <TableCell>{r.rule_name}</TableCell>
-              <TableCell>
-                <pre className="whitespace-pre-wrap text-xs m-0">
-                  {JSON.stringify(r.result, null, 2)}
-                </pre>
-              </TableCell>
+const RuleResultsTable: React.FC<Props> = ({ results, allRules, selectedRules, onSelectedRulesChange }) => {
+  const filteredResults = React.useMemo(
+    () => results.filter(r => selectedRules.length === 0 || selectedRules.includes(r.rule_name)),
+    [results, selectedRules]
+  );
+
+  return (
+    <div>
+      <h3 className="text-xl font-semibold mb-2">Latest Rule Results</h3>
+      <Autocomplete
+        multiple
+        size="small"
+        options={allRules}
+        value={selectedRules}
+        onChange={(_, v) => onSelectedRulesChange(v as string[])}
+        renderInput={(params) => <TextField {...params} label="Filter rules" placeholder="All" />}
+        sx={{ mb: 1, width: 300 }}
+      />
+      <TableContainer component={Paper}>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Detected At</TableCell>
+              <TableCell>Rule Name</TableCell>
+              <TableCell>Result</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  </div>
-);
+          </TableHead>
+          <TableBody>
+            {filteredResults.map(r => (
+              <TableRow key={r.id}>
+                <TableCell>{new Date(r.detected_at).toLocaleString()}</TableCell>
+                <TableCell>{r.rule_name}</TableCell>
+                <TableCell>
+                  <pre className="whitespace-pre-wrap text-xs m-0">
+                    {JSON.stringify(r.result, null, 2)}
+                  </pre>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
 
 export default RuleResultsTable;
 
