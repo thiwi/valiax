c86cb02e7a3ee5f460c6f2df94806bdb5aa1c9c5
Thilo Wilts <thilowilts@googlemail.com>
2025-07-22 20:26:47 +0200

    Revert "Add SwiftUI iOS app with mocked data"
    This reverts commit 721d7201f794c42ee8d5f5aab0c5d33a0045312e.

--- ios/Sources/LumaIOS/Models/Rule.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct Rule: Identifiable, Decodable {
-    let id: UUID
-    let title: String
-    let description: String
-}
--- ios/Tests/LumaIOSTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import LumaIOS
-
-final class LumaIOSTests: XCTestCase {
-    func testLoadMockData() {
-        let viewModel = RuleListViewModel()
-        XCTAssertFalse(viewModel.rules.isEmpty, "Rules should load from mock data")
-    }
-}
